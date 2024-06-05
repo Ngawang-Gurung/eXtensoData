a736fe8c2ae6bc08b20b8de59dd2845bd80ff0ad
Ngawang Gurung <67688107+Ngawang-Gurung@users.noreply.github.com>
2024-06-05 21:53:37 +0545

    modified data_integrity files

--- Python Tasks/data_integrity.py
+++ Python Tasks/data_integrity.py
@@ -5,23 +5,38 @@ class DataFrameIntegrity:
     
     def __init__(self, df):
         self.df = df.copy()
-        
-    def null_check(self, column=None):
         self.replace_disguised_missing()
+        
+    def null_check(self, column = None):
         if column:
             null_count = self.df[column].isna().sum()
-            result = pd.DataFrame({column: [null_count]}, index=['null_count'])
+            null_records = self.df[self.df[column].isna()]
         else:
-            null_count = self.df.isna().sum()
-            result = pd.DataFrame(null_count, columns=['null_count']).transpose()
-        return result
+            null_count = self.df.isna().sum().sum()
+            null_records = self.df[self.df.isna().any(axis=1)]
 
-    def duplicate_check(self, column):
-        duplicate_count = self.df.duplicated(subset=[column]).sum()
+        if null_count !=0:
+            if column:
+                print(f"\n {null_count} null values found in column {column}\n")
+            else:
+                print(f"\n{null_count} null records found in the dataframe\n")
+
+        return null_records
+    
+    def duplicate_check(self, column = None):
+        if column:
+            duplicate_count = self.df.duplicated(subset=[column]).sum()
+            duplicate_records = self.df[self.df.duplicated(subset=[column], keep=False)]
+        else:
+            duplicate_count = self.df.duplicated().sum()
+            duplicate_records = self.df[self.df.duplicated(keep=False)]
+
         if duplicate_count != 0:
-            print(f"\nNo. of duplicated records in column {column} is {duplicate_count}")
-        
-        duplicate_records = self.df[self.df.duplicated(subset=[column], keep=False)]
+            if column:
+                print(f"\n{duplicate_count} duplicate values found in column '{column}'\n")
+            else:
+                print(f"\n{duplicate_count} duplicate values found in the dataframe\n") 
+
         return duplicate_records
     
     def negative_check(self, column):
@@ -29,37 +44,31 @@ class DataFrameIntegrity:
         negative_records = self.df[numeric_column < 0]
         negative_count = negative_records.shape[0]
         if not negative_records.empty:
-            print(f"\n{negative_count} Negative values found in column {column}")
+            print(f"\n{negative_count} negative values found in column {column}\n")
         return negative_records
 
-    def show_missing_records(self):
-        self.replace_disguised_missing()
-        return self.df[self.df.isna().any(axis=1)]
-    
-    def find_duplicate_row_count(self):
-        return self.df.duplicated().sum()
-    
     def type_check(self, column, data_type):
         invalid_records = pd.DataFrame()
-        
+        invalid_counts = 0
+
         if data_type == 'int':
             invalid_records = self.df[self.df[column].apply(lambda x: not isinstance(x, int))]
             invalid_counts = invalid_records.shape[0]
-            print(f"\n{invalid_counts} Non-integer values found in column {column}")
         elif data_type == 'str':
             invalid_records = self.df[self.df[column].apply(lambda x: not isinstance(x, str))]
             invalid_counts = invalid_records.shape[0]
-            print(f"\n{invalid_counts} Non-string values found in column {column}")
         elif data_type == 'datetime':
             try:
                 self.df[column] = pd.to_datetime(self.df[column], errors='raise')
             except ValueError:
                 invalid_records = self.df[pd.to_datetime(self.df[column], errors='coerce').isna()]
                 invalid_counts = invalid_records.shape[0]
-                print(f"\n{invalid_counts} Invalid date values found in column {column}")
         else:
             print("\nInvalid data type provided. Enter either 'int', 'str', or 'datetime'.")
-            return
+            return None
+       
+        if invalid_counts:
+            print(f"\n{invalid_counts} invalid '{data_type}' values found in column '{column}'\n")
         
         return invalid_records
     
@@ -72,28 +81,21 @@ df = pd.DataFrame( {
     'DOB': ['2020-01-01', '2021-12-31', 'June 5, 2024', '2021-12-31', '!']
 })
 
-integrity_checker = DataFrameIntegrity(df)
-
-# print(integrity_checker.null_check())
-# print(integrity_checker.null_check(column='account_number'))
-
-# print(integrity_checker.duplicate_check(column='account_number'))
-
-# print(integrity_checker.find_duplicate_row_count())
-# print(integrity_checker.show_missing_records())
-
-# print(integrity_checker.negative_check('account_number'))
-
-# print(integrity_checker.type_check(column='account_number', data_type='int'))
-# print(integrity_checker.type_check(column='DOB', data_type='datetime'))
-
-# Domain Constraints
 expected_types = {
     'account_number': 'int' ,
     'user': 'str',
     'DOB': 'datetime'
 }
 
-for column in df.columns:
-    if column in expected_types:
-        print(f"{integrity_checker.type_check(column, expected_types[column])}\n")
+integrity_checker = DataFrameIntegrity(df)
+
+# print(integrity_checker.null_check())
+print(integrity_checker.null_check(column='account_number'))
+
+# print(integrity_checker.duplicate_check())
+print(integrity_checker.duplicate_check(column='account_number'))
+
+print(integrity_checker.negative_check('account_number'))
+
+print(integrity_checker.type_check(column='account_number', data_type='int'))
+print(integrity_checker.type_check(column='DOB', data_type='datetime'))
