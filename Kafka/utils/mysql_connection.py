87b48e5899de0d0f5aaed4783a34f35b74bb8892
Ngawang-Gurung <tseringnc707@gmail.com>
2024-07-23 10:03:00 +0545

    refactor: added comments on RFM Analysis and Data Validation, edited mysql_connection

--- Kafka/utils/mysql_connection.py
+++ Kafka/utils/mysql_connection.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 import os
 from dotenv import load_dotenv
+import logging
 
 load_dotenv()
 
@@ -11,6 +12,9 @@ DB_USERNAME = os.getenv('DB_USERNAME')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = int(os.getenv('DB_PORT'))
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def table_df(database_name, table_name):
     conn_url = URL.create(
@@ -23,24 +27,32 @@ def table_df(database_name, table_name):
     )
 
     engine = create_engine(conn_url)
+
     con = engine.connect()
 
     query = f'SELECT * FROM {table_name}'
-    df = pd.read_sql(sql=query, con=con)
-    con.close()  # Close the connection after use
+    # df = pd.read_sql(sql=query, con=con)                  # Pandas < 2.2.0
+    df = pd.read_sql(sql=query, con=con.connection)         # Pandas >= 2.2.0
+    con.close()  
     return df
 
 def df_table(dataframe, database_name, table_name):
-    conn_url = URL.create(
-        "mysql+mysqlconnector",
-        username=DB_USERNAME,
-        password=DB_PASSWORD,
-        host=DB_HOST,
-        port=DB_PORT,
-        database=database_name
-    )
+    try:
+        conn_url = URL.create(
+            "mysql+mysqlconnector",
+            username=DB_USERNAME,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT,
+            database=database_name
+        )
 
-    engine = create_engine(conn_url)
-    con = engine.connect()
-    dataframe.to_sql(table_name, con=con, if_exists='replace', index=False)
-    con.close()  # Close the connection after use
+        engine = create_engine(conn_url)
+        dataframe.to_sql(table_name, con=engine, if_exists='replace', index=False, chunksize=100000) 
+
+        # with engine.connect() as con:
+        #     # dataframe.to_sql(table_name, con=con, if_exists='replace', index=False, chunksize=100000)  # Pandas < 2.2.0
+        #     dataframe.to_sql(table_name, con=con.connection, if_exists='replace', index=False, chunksize=100000)  # Pandas < 2.2.0
+
+    except Exception as e:
+        logger.error(f"Error occurred: {e}")
